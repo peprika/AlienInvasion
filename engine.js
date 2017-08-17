3083092f19df0b67b943c8f2025b752826da421a
Riku Pepponen <riku.pepponen@gmail.com>
2017-08-18 00:18:44 +0300

    Improve code's readability

--- engine.js
+++ engine.js
@@ -2,7 +2,7 @@
 var Game = new function() {
 
 	// Game initialization
-	this.initialize = function(canvasElementId,sprite_data,callback) {
+	this.initialize = function(canvasElementId, sprite_data, callback) {
 
 		this.canvas = document.getElementById(canvasElementId);
 		this.width = this.canvas.width;
@@ -61,14 +61,14 @@ this.loop = function() {
 // The SpriteSheet class
 var SpriteSheet = new function() {
  this.map = { };
- this.load = function(spriteData,callback) {
+ this.load = function(spriteData, callback) {
 	 this.map = spriteData;
 	 this.image = new Image();
 	 this.image.onload = callback;
 	 this.image.src = 'images/sprites.png';
  };
  
- this.draw = function(ctx,sprite,x,y,frame) {
+ this.draw = function(ctx, sprite, x, y, frame) {
 	 var s = this.map[sprite];
 	 if(!frame) frame = 0;
 	 ctx.drawImage(this.image,
--- game.js
+++ game.js
@@ -18,7 +18,7 @@ var playGame = function() {
 }
 
 // Starfield
- var Starfield = function(speed,opacity,numStars,clear) {
+ var Starfield = function(speed, opacity, numStars, clear) {
 	 
 	// Setup the offscreen canvas
 	var stars = document.createElement("canvas");
@@ -31,21 +31,23 @@ var playGame = function() {
 	// If the clear option is set, make the background black instead of transparent
 	if(clear) {
 		starCtx.fillStyle = "#000";
-		starCtx.fillRect(0,0,stars.width,stars.height);
+		starCtx.fillRect(0, 0, stars.width, stars.height);
 	}
 	
 	// Now dra a bunch of random 2 pixel rectangles onto the offscreen canvas
 	starCtx.fillStyle = "#FFF";
 	starCtx.globalAlpha = opacity;
 	for(var i = 0; i < numStars; i ++) {
-		starCtx.fillRect(Math.floor(Math.random()*stars.width), Math.floor(Math.random()*stars.height), 2, 2);
+		starCtx.fillRect(Math.floor(Math.random()*stars.width),
+						 Math.floor(Math.random()*stars.height),
+						 2, 2);
 	}
 	
 	// This method is called every frame to draw the starfield onto the canvas
 	this.draw = function(ctx) {
 		var intOffset = Math.floor(offset);
 		var remaining = stars.height - intOffset;
-		// Draw the topp half of the starfield
+		// Draw the top half of the starfield
 		if(intOffset > 0) {
 			ctx.drawImage(stars, 0, remaining, stars.width, intOffset, 0, 0, stars.width, intOffset);
 		}
