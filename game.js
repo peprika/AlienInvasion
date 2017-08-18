2a69deb7c2b9ac315f9b22bac820fa5c2f7eb2b1
Riku Pepponen <riku.pepponen@gmail.com>
2017-08-18 17:34:34 +0300

    Add 2 enemies to the screen

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 var sprites = {
  ship: { sx: 1, sy: 0, w: 37, h: 42, frames: 1 },
- missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 }
+ missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+ enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+ enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
+ enemy_ship: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
+ enemy_circle: { sx: 158, sy: 0, w: 32, h: 33, frames: 1 }
+};
+
+var enemies = {
+	basic: { x: 100, y: -50, sprite:'enemy_purple', B: 100, C: 2, E: 100 }
 };
 
 var startGame = function() {
@@ -16,6 +24,8 @@ window.addEventListener("load", function() {
 
 var playGame = function() {
 	var board = new GameBoard();
+	board.add(new Enemy(enemies.basic));
+	board.add(new Enemy(enemies.basic, { x: 200 }));
 	board.add(new PlayerShip());
 	Game.setBoard(3, board);
 }
